f499206757dc17cd58082c826ff47f05a5c689b3
leowolf749 <leowolf749@gmail.com>
2016-12-13 15:57:39 -0500

    Started html and js

---
+++ homework.js
@@ -0,0 +1,39 @@
+function init() {
+
+    let button = document.querySelector('#new');
+    button.addEventListener('click', function() {
+
+        let child = document.createElement('li');
+
+
+        let image = document.createElement('img');
+        image.src = 'https://randomuser.me/api/portraits/men/90.jpg';
+        child.appendChild(image);
+
+        let heading = document.createElement('h2');
+        heading.textContent = 'Jimothy Gramble';
+        child.appendChild(heading);        
+
+        let like = document.createElement('button');
+        child.appendChild(like);
+        like.textContent = 'Like';
+        like.addEventListener('click', function(){
+            console.log('Likes ' + heading.textContent)
+        })
+
+        let nope = document.createElement('button');
+        child.appendChild(nope);
+        nope.textContent = 'Nope';
+            nope.addEventListener('click', function(){
+                console.log(heading.textContent + ' has been noped')
+            })
+
+        let parent = document.querySelector('#bio');
+        parent.appendChild(child);
+
+    });
+
+
+} 
+
+window.addEventListener('load', init);
